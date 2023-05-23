2a704a87d6a4e28e153fb36b66664bfa28ee8226
Notwacob <wilson.a.jacob@gmail.com>
2023-05-23 05:22:39 -0400

    Update 5:22 AM - 5/23/2023

--- passport-config.js
+++ passport-config.js
@@ -1,17 +1,17 @@
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
+const LocalStrategy = require("passport-local").Strategy
+const bcrypt = require("bcrypt")
 
-function initialize(passport) {
-    // Function to authenticate users
+function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUsers = async (email, password, done) => {
-        // Get users by email
-        const user = getUserByEmail(email)
-        if (user == null) {
+        const user = await getUserByEmail(email)
+        if (user == null){
             return done(null, false, {message: "No user found with that email"})
         }
         try {
-            if (await bcrypt.compare(password, user.password)) {
+            if(await bcrypt.compare(password, user.password)){
                 return done(null, user)
+            } else{
+                return done(null, false, {message: "Password Incorrect"})
             }
         } catch (e) {
             console.log(e);
@@ -19,9 +19,18 @@ function initialize(passport) {
         }
     }
 
-    passport.use(new LocalStrategy({usernameField: 'email'}))
-    passport.serialzeUser((user, done) => {})
-    passport.deserialzeUser((id, done) => {})
+    passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUsers))
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await getUserById(id);
+            done(null, user);
+        } catch (error) {
+            done(error);
+        }
+    });
 }
 
 module.exports = initialize
--- server.js
+++ server.js
@@ -1,64 +1,127 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
 
-// Importing Libraries that we installed using npm
-const express = require('express')
-const app = express()
-
+// Import all necessary packages
+const express = require("express");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt') // Importing bcrypt package
-const passport = require('passport')
-const initializePassport = require("./passport-config")
+const initializePassport = require("./passport-config");
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const app = express();
 
-mongoose.connect(
-    MONGODB_URL, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
-
-const usersSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     id: String,
     name: String,
     email: String,
-    password: String
+    password: String,
 });
 
-const users = mongoose.model('users', usersSchema);
+const users = mongoose.model('users', userSchema);
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected...'))
+
+async function getUserByEmail(email) {
+    return await users.findOne({ email: email }).catch(err => {
+        console.error('Error occurred:', err);
+    });
+}
+
+async function getUserById(id) {
+    return await users.findOne({ id: id });
+  }
+
+initializePassport(passport, getUserByEmail, getUserById, users);
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); // make sure your EJS templates are in a directory named "views" in the same directory as your server.js file
 
 app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // We wont resave the session variable if nothing is changed
+    saveUninitialized: false
+}))
+app.use(passport.initialize()) 
+app.use(passport.session())
+app.use(methodOverride("_method"))
 
-app.post("/register", async (req, res) => {
+// Configuring the register post functionality
+app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+}))
+
+// Configuring the register post functionality
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        // Create a new user
         const user = new users({
             id: Date.now().toString(),
             name: req.body.name,
             email: req.body.email,
             password: hashedPassword
         })
+
+        // Save the user to the database
         await user.save()
+
+        console.log(users); // Display newly registered in the console
         res.redirect("/login")
-    } catch(e) {
-        console.log(e)
+        
+    } catch (e) {
+        console.log(e);
         res.redirect("/register")
     }
 })
 
 // Routes
-app.get('/', (req, res) => {
-    res.render("index.ejs")
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render("index.ejs", {name: req.user.name})
 })
 
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render("login.ejs")
 })
 
-app.get('/register', (req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render("register.ejs")
 })
 // End Routes
+
+app.delete("/logout", (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
+});
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/login")
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect("/")
+    }
+    next()
+}
 
 app.listen(3000, () => console.log("Server is running"));
